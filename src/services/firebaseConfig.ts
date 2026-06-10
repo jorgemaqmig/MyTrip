8ce03cc9a23c58b9c200f2ae4ec1a91e6ee1906e
jorgemaqmig <24jmm07@gmail.com>
2026-06-10 19:13:35 +0200

    Comentarios firebaseConfig

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { 
   initializeAuth, 
   getAuth, 
-  // @ts-ignore - A veces los tipos de Firebase no exportan esto correctamente en el punto de entrada principal
+  // @ts-ignore
   getReactNativePersistence 
 } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-// Tu configuración de Firebase desde variables de entorno (.env)
+// Configuración de Firebase
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || "",
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
@@ -20,10 +20,9 @@ const firebaseConfig = {
   measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
 };
 
-// Inicializar Firebase
+// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
 
-// Inicializar Auth con persistencia en React Native (Evitando doble inicialización)
 let auth: any;
 try {
   auth = initializeAuth(app, {
@@ -35,7 +34,6 @@ try {
 
 export { auth };
 export const db = getFirestore(app);
-// Inicializamos Storage apuntando al bucket correcto
 export const storage = getStorage(app, "gs://mytrip-6adc8.firebasestorage.app");
 
 export default app;
